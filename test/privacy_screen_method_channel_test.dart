f6a355548fc3fcf6c95af8692f620a248835ec06
Edward <edward.yuan@iotpay.ca>
2022-08-16 13:38:25 -0700

    update versioning

--- test/privacy_screen_method_channel_test.dart
+++ test/privacy_screen_method_channel_test.dart
@@ -1,24 +1,24 @@
-import 'package:flutter/services.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:privacy_screen/src/privacy_screen_method_channel.dart';
+// import 'package:flutter/services.dart';
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:privacy_screen/src/privacy_screen_method_channel.dart';
 
-void main() {
-  MethodChannelPrivacyScreen platform = MethodChannelPrivacyScreen();
-  const MethodChannel channel = MethodChannel('privacy_screen');
+// void main() {
+//   MethodChannelPrivacyScreen platform = MethodChannelPrivacyScreen();
+//   const MethodChannel channel = MethodChannel('privacy_screen');
 
-  TestWidgetsFlutterBinding.ensureInitialized();
+//   TestWidgetsFlutterBinding.ensureInitialized();
 
-  setUp(() {
-    channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
-    });
-  });
+//   setUp(() {
+//     channel.setMockMethodCallHandler((MethodCall methodCall) async {
+//       return '42';
+//     });
+//   });
 
-  tearDown(() {
-    channel.setMockMethodCallHandler(null);
-  });
+//   tearDown(() {
+//     channel.setMockMethodCallHandler(null);
+//   });
 
-  // test('getPlatformVersion', () async {
-  //   expect(await platform.getPlatformVersion(), '42');
-  // });
-}
+//   // test('getPlatformVersion', () async {
+//   //   expect(await platform.getPlatformVersion(), '42');
+//   // });
+// }
